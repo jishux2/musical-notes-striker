0044f9b29e41924b89fd08c7a75f122f82464a83
jishux <3418168893@qq.com>
2023-09-10 23:12:56 +0800

    修改

--- audioPlayer.js
+++ audioPlayer.js
@@ -8,6 +8,8 @@ for (var i = 1; i <= audioCount; i++) {
     audioFiles.push("audio" + i + ".flac");
 }
 
+// 这是当前要播放的音频元素
+var currentAudio = new Audio();
 // 这是当前要播放的音频文件的索引
 var currentIndex = 0;
 // 这是一个音频元素的数组，用来存储所有的音频文件
@@ -32,12 +34,38 @@ preloader.preload = "auto";
 
 // 这是一个函数，用来播放下一个音频文件
 function playNext() {
-    // 停止当前播放的音频
-    audio[currentIndex - 1].pause();
-    // 从数组中获取当前要播放的音频元素
-    audio[currentIndex].play();
+    // 如果还有音频不能播放，就不做任何事情，并显示一个提示信息
+    if (!allCanPlay) {
+        alert("请稍等一下，还有音频在加载中...");
+        return;
+    }
+    currentAudio.pause();
+    // 获取当前要播放的音频元素
+    currentAudio = audio[currentIndex]
+    // 播放当前的音频元素
+    currentAudio.play();
     // 增加索引，或者如果到达数组的末尾就重置为零
     currentIndex = (currentIndex + 1) % audioFiles.length;
+}
+
+// 这是一个变量，用来记录是否所有音频都可以播放
+var allCanPlay = false;
+// 这是一个函数，用来检查是否所有音频都可以播放，并设置allCanPlay变量为真
+function checkAllCanPlay() {
+    // 用一个循环来遍历每个音频元素
+    for (var i = 0; i < audio.length; i++) {
+        // 如果有一个音频元素还不能播放，就返回，不做任何事情
+        if (audio[i].readyState < 4) {
+            return;
+        }
+    }
+    // 如果所有音频元素都可以播放，就设置allCanPlay变量为真，并显示一个提示信息
+    allCanPlay = true;
+    alert("所有音频都可以播放了，点击任何地方来开始吧！");
+}
+// 给每个音频元素添加一个事件监听器，当它们可以播放时，就调用checkAllCanPlay函数
+for (var i = 0; i < audio.length; i++) {
+    audio[i].addEventListener("canplaythrough", checkAllCanPlay);
 }
 
 // 给文档添加一个事件监听器，当它被点击时，就调用playNext函数
